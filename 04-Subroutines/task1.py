97057a0ea16d3f77abe9ee27730cffbd5663ef04
Boykisser <234189@student.uek.krakow.pl>
2023-11-05 01:32:14 +0100

    commit

---
+++ 04-Subroutines/task1.py
@@ -0,0 +1,18 @@
+#Using built-in Python functions, write a program that calculates and displays:
+#        a. length of the phrase: "computer science"
+#        b. letter read from the keyboard
+#        c. string representing the number 5068
+#        d. numeric representing the string "20303"
+#        e. the smallest number given: 4,7,2,3,9,8
+
+a = len('computer science')
+b = input()
+c = str(5068)
+d = int('20303')
+e = min(4,7,2,3,9,8)
+
+print(f"a: {a}")
+print(f"b: {b}")
+print(f"c: {c}")
+print(f"d: {d}")
+print(f"e: {e}")
