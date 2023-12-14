3e117953fbe64f5756cdc635d85a94f14654404a
Boykisser <234189@student.uek.krakow.pl>
2023-12-14 22:59:47 +0100

    hh

---
+++ 06-Arrays/p9.py
@@ -0,0 +1,30 @@
+array = [2,3,7,5,4]
+
+#a
+print(array)
+
+#b
+print(len(array))
+
+#c
+print(array[0])
+
+#d
+print(array[1])
+
+#e
+print(array[len(array)-1])
+
+#f
+print(array[len(array)-2])
+
+#g
+print(array[len(array)-1]+array[0])
+
+#h
+print(array[len(array)//2])
+
+#i
+for n in array:
+    print(n, end=" ") #tak się zastępuje znak końca linii innym znakiem
+
